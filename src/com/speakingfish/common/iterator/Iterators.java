277658fa001ac940bb963244c271ce0567071528
borka <borka@localhost>
2016-04-12 01:14:08 +0300

    update

--- src/com/speakingfish/common/iterator/Iterators.java
+++ src/com/speakingfish/common/iterator/Iterators.java
@@ -1,16 +1,24 @@
 package com.speakingfish.common.iterator;
 
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
+import com.speakingfish.common.Maps;
 import com.speakingfish.common.annotation.Compatibility.*;
 import com.speakingfish.common.function.Acceptor;
+import com.speakingfish.common.function.Creator;
 import com.speakingfish.common.function.Getter;
 import com.speakingfish.common.function.Mapper;
 import com.speakingfish.common.mapper.Mappers;
 import com.speakingfish.common.type.Typecasts;
 import com.speakingfish.common.type.Typed;
+
+import static com.speakingfish.common.collection.CollectionHelper.*;
+import static com.speakingfish.common.Maps.*;
 
 public class Iterators {
     
@@ -104,6 +112,37 @@ public class Iterators {
         return new RepeatGenerator<T>(count, value);
     }
 
+    public static <
+        T_DEST_COLLECTION extends Collection<T_DEST_ITEM>,
+        T_DEST_ITEM,
+        T_SRC_ITEM
+    > Creator<T_DEST_COLLECTION, Iterator<T_SRC_ITEM>> collectCreator(
+        final Getter<T_DEST_COLLECTION> creator,
+        final Mapper<T_DEST_ITEM, T_SRC_ITEM> mapper
+    ) {
+        return new Creator<T_DEST_COLLECTION, Iterator<T_SRC_ITEM>>() {
+            public T_DEST_COLLECTION apply(Iterator<T_SRC_ITEM> src) {
+                return collect(creator.get(), mapIterator(src, mapper));
+            }
+        };
+    }
+
+    public static <
+        T_Map__T_KEY_T_DEST_VALUE extends Map<T_KEY, T_DEST_VALUE>,
+        T_KEY                                                     ,
+        T_DEST_VALUE                                              ,
+        T_SRC_VALUE
+    > Creator<T_Map__T_KEY_T_DEST_VALUE, Iterator<Entry<T_KEY, T_SRC_VALUE>>> collectMapValueCreator(
+        final Getter<T_Map__T_KEY_T_DEST_VALUE> creator,
+        final Mapper<T_DEST_VALUE, T_SRC_VALUE> mapper
+    ) {
+        return new Creator<T_Map__T_KEY_T_DEST_VALUE, Iterator<Entry<T_KEY, T_SRC_VALUE>>>() {
+            public T_Map__T_KEY_T_DEST_VALUE apply(Iterator<Entry<T_KEY, T_SRC_VALUE>> src) {
+                return collectMap(creator.get(), mapIterator(src, Maps.<T_KEY, T_DEST_VALUE, T_SRC_VALUE>makeEntryValueMapper(mapper)));
+            }
+        };
+    }
+    
     static { Dummy.dummy(); }
 
 }
